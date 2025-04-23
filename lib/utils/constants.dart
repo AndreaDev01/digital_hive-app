bc5bff15bbb78b3166da2f351f2889041dff22a9
Andreaaas <andreadevelopersalvatore@gmail.com>
2025-04-23 21:17:55 +0200

    init project

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+final Color first_color = Colors.black;
+final Color secondary_color = Colors.orange;
---
+++ lib/views/hives.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class Hives extends StatefulWidget {
+  const Hives({super.key});
+
+  @override
+  State<Hives> createState() => _HivesState();
+}
+
+class _HivesState extends State<Hives> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Hives"),
+    );
+  }
+}
